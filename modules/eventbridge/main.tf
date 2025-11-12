f9b83e1711bbde70fcb58bb197731de8d76b2338
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-11-12 15:49:50 +0300

    add explicit event bus policy for S3 trigger

--- modules/eventbridge/main.tf
+++ modules/eventbridge/main.tf
@@ -37,3 +37,37 @@ resource "aws_sqs_queue_policy" "eventbridge_to_sqs" {
     ]
   })
 }
+
+# This policy is crucial. It gives the S3 service principal permission to publish
+# events to the default EventBridge bus, but scoped down to only allow events
+# that originate from our specified source buckets.
+resource "aws_cloudwatch_event_bus_policy" "s3_to_eventbridge" {
+  event_bus_name = "default" # We are using the default event bus
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AllowS3ToPutEvents",
+        Effect    = "Allow",
+        Principal = {
+          Service = "s3.amazonaws.com"
+        },
+        Action    = "events:PutEvents",
+        Resource  = "arn:aws:events:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:event-bus/default",
+        Condition = {
+          "ArnLike" = {
+            "aws:SourceArn" = [for arn in var.event_source_arns : "${arn}"]
+          }
+        }
+      }
+    ]
+  })
+
+  # We add a dependency on the rule to ensure the policy isn't created in isolation.
+  depends_on = [aws_cloudwatch_event_rule.s3_creation_rule]
+}
+
+# ADD THESE DATA SOURCES to construct the policy ARN dynamically
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
